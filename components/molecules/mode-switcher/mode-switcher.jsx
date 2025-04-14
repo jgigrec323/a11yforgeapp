8d6814a849cbd591535414c196e242932ef34720
Jean Galant Yombouno <jeangalanty@gmail.com>
2025-04-14 23:10:41 +0300

    Refactor styles: consolidate global styles, enhance dark mode support, and clean up mode switcher component

--- components/molecules/mode-switcher/mode-switcher.jsx
+++ components/molecules/mode-switcher/mode-switcher.jsx
@@ -1,45 +1,47 @@
-'use client'
-
-import { useEffect, useState } from 'react'
+"use client";
+import { useEffect, useState } from "react";
 
 const ModeSwitcher = () => {
-    const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
 
-    useEffect(() => {
-        // Check localStorage for saved theme preference
-        const savedTheme = localStorage.getItem('theme')
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const isDark = savedTheme === "dark" || (!savedTheme && prefersDark);
 
-        if (
-            savedTheme === 'dark' ||
-            (!savedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches)
-        ) {
-            setDarkMode(true)
-            document.documentElement.classList.add('dark')
-        }
-    }, [])
+    setDarkMode(isDark);
 
-    const toggleDarkMode = () => {
-        const newMode = !darkMode
-
-        setDarkMode(newMode)
-
-        if (newMode) {
-            document.documentElement.classList.add('dark')
-            localStorage.setItem('theme', 'dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-            localStorage.setItem('theme', 'light')
-        }
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
+  }, []);
 
-    return (
-        <button
-            title="mode switcher"
-            onClick={toggleDarkMode}
-            className={`mode-switcher ${darkMode ? 'dark' : ''}`}>
-            <div className={`toggle-circle ${darkMode ? 'dark active' : ''}`}></div>
-        </button>
-    )
-}
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
 
-export default ModeSwitcher
+    if (newMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  };
+
+  return (
+    <button
+      title="mode switcher"
+      onClick={toggleDarkMode}
+      className={`mode-switcher ${darkMode ? "dark" : ""}`}
+    >
+      <div className={`toggle-circle ${darkMode ? "dark active" : ""}`}></div>
+    </button>
+  );
+};
+
+export default ModeSwitcher;
